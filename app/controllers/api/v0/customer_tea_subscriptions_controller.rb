b2be94cc873280c02dbc590e18c5d93a21eead5b
Grace Joh <graceehjoh@gmail.com>
2023-08-03 15:24:41 -0500

    feat: create CustomerTeaSubscriptionsController update action

--- app/controllers/api/v0/customer_tea_subscriptions_controller.rb
+++ app/controllers/api/v0/customer_tea_subscriptions_controller.rb
@@ -1,9 +1,16 @@
 class Api::V0::CustomerTeaSubscriptionsController < ApplicationController
-  rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid
+  rescue_from ActiveRecord::RecordInvalid, with: :render_error_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_error_response
 
   def create
     @sub = CustomerTeaSubscription.create!(customer_tea_subscription_params)
     render json: CustomerTeaSubscriptionSerializer.new(@sub), status: 201
+  end
+
+  def update
+    @sub = CustomerTeaSubscription.find_by!(customer_tea_subscription_params)
+    @sub.cancelled!
+    render json: CustomerTeaSubscriptionSerializer.new(@sub), status: 200
   end
 
   private
@@ -12,7 +19,7 @@ class Api::V0::CustomerTeaSubscriptionsController < ApplicationController
     params.permit(:customer_id, :tea_id, :subscription_id)
   end
 
-  def render_record_invalid(exception)
+  def render_error_response(exception)
     render json: ErrorSerializer.serialize(exception, 404), status: 404
   end
 end
