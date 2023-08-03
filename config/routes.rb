a9f4ad61fb92c7734e16ee31dcde6817b0a65687
Grace Joh <graceehjoh@gmail.com>
2023-08-03 15:23:45 -0500

    feat: create patch subscriptions route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       post '/subscriptions', to: 'customer_tea_subscriptions#create'
+      patch '/subscriptions', to: 'customer_tea_subscriptions#update'
     end
   end
 end
